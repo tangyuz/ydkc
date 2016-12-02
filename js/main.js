44a9fb19916f91f0ee710e4478ec3da714a5e94c
aaa <aaa>
2016-12-02 22:41:23 +0800

    Signed-off-by: aaa <aaa>

--- js/main.js
+++ js/main.js
@@ -37,113 +37,112 @@
 		
 
 
-//if(/aboutus/.test(location.href)){
-//	$("#header").load("head.html",function(){
-//		
-//	})
-//	
-//	$(".footer").load("footer.html",function(){
-//		
-//	})
-//}
-//
-//
-//if(/news/.test(location.href)){
-//	$("#header").load("head.html",function(){
-//		
-//	})
-//	
-//	$(".footer").load("footer.html",function(){
-//		
-//	})
-//}
-//
-//
-//if(/consu/.test(location.href)){
-//	$("#header").load("head.html",function(){
-//		
-//	})
-//	
-//	$(".footer").load("footer.html",function(){
-//		
-//	})
-//}
-//
-//
-//
-//
-//if(/rem/.test(location.href)){
-//	$("#header").load("head.html",function(){
-//		
-//	})
-//	
-//	$(".footer").load("footer.html",function(){
-//		
-//	})
-//}
-//
-//if(/coo/.test(location.href)){
-//	$("#header").load("head.html",function(){
-//		
-//	})
-//	
-//	$(".footer").load("footer.html",function(){
-//		
-//	})
-//	
-//	 $("#capslide_img_cont").capslide({
-//      caption_color: 'white',
-//      caption_bgcolor: 'black',
-//      overlay_bgcolor: 'black',
-//      border: '',
-//      showcaption: false
-//  });
-//  $(".move").mouseover(function(){
-//  	$(this).find(".pc").stop(true).slideDown();
-//  })
-//  $(".move").mouseout(function(){
-//  	$(this).find(".pc").stop(true).slideUp();
-//  })
-//	
-//jQuery(".ruwangyiyuan").slide({
-//  titCell: ".hdm ul",
-//  mainCell: ".bdm1 ul",
-//  autoPage: true,
-//  effect: "left",
-//  autoPlay: true,
-//  vis: 2
-//});
-//
-//
-//$(function() {
-//  $(".ic_container").capslide({
-//      caption_color: 'white',
-//      caption_bgcolor: 'black',
-//      overlay_bgcolor: 'black',
-//      border: '',
-//      showcaption: false
-//  });
-//});
-//	
-//	
-//}
-//
-//if(/sma/.test(location.href)){
-//	$("#header").load("head.html",function(){
-//		
-//	})
-//	
-//	$(".footer").load("footer.html",function(){
-//		
-//	})
-//}
-//
-//})
-//	
-//	 
-//
-//
-//
-//	
-// 
-//
+if(/aboutus/.test(location.href)){
+	$("#header").load("head.html",function(){
+		
+	})
+	
+	$(".footer").load("footer.html",function(){
+		
+	})
+}
+
+
+if(/news/.test(location.href)){
+	$("#header").load("head.html",function(){
+		
+	})
+	
+	$(".footer").load("footer.html",function(){
+		
+	})
+}
+
+
+if(/consu/.test(location.href)){
+	$("#header").load("head.html",function(){
+		
+	})
+	
+	$(".footer").load("footer.html",function(){
+		
+	})
+}
+
+
+
+
+if(/rem/.test(location.href)){
+	$("#header").load("head.html",function(){
+		
+	})
+	
+	$(".footer").load("footer.html",function(){
+		
+	})
+}
+
+if(/coo/.test(location.href)){
+	$("#header").load("head.html",function(){
+		
+	})
+	
+	$(".footer").load("footer.html",function(){
+		
+	})
+	
+	 $("#capslide_img_cont").capslide({
+        caption_color: 'white',
+        caption_bgcolor: 'black',
+        overlay_bgcolor: 'black',
+        border: '',
+        showcaption: false
+    });
+    $(".move").mouseover(function(){
+    	$(this).find(".pc").stop(true).slideDown();
+    })
+    $(".move").mouseout(function(){
+    	$(this).find(".pc").stop(true).slideUp();
+    })
+	
+jQuery(".ruwangyiyuan").slide({
+    titCell: ".hdm ul",
+    mainCell: ".bdm1 ul",
+    autoPage: true,
+    effect: "left",
+    autoPlay: true,
+    vis: 2
+});
+
+
+$(function() {
+    $(".ic_container").capslide({
+        caption_color: 'white',
+        caption_bgcolor: 'black',
+        overlay_bgcolor: 'black',
+        border: '',
+        showcaption: false
+    });
+});
+	
+	
+}
+
+if(/sma/.test(location.href)){
+	$("#header").load("head.html",function(){
+		
+	})
+	
+	$(".footer").load("footer.html",function(){
+		
+	})
+}
+
+})
+	
+	 
+
+
+
+	
+   
