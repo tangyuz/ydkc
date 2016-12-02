05fa73fb4a37d75a21c5a06afcaab08f328dd72b
aaa <aaa>
2016-12-02 22:17:47 +0800

    Signed-off-by: aaa <aaa>

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 
 
-if(/index/.test(location.href)){
+
 	alert(1)
-	}
-////		$("#header").load("head.html",function(){
-////			})
-////	
-////	  	$(".footer").load("footer.html",function(){
-////	  	})
+	
+//		$("#header").load("head.html",function(){
+//			})
 //	
-// 
-//	$(".slideBox").slide({
-//  	mainCell: ".bd ul",
-//  	autoPlay: true
-//		});
-//		jQuery(".focusBox").slide({
-//		    titCell: ".num li",
-//		    mainCell: ".pic",
-//		    effect: "fold",
-//		    autoPlay: true,
-//		    trigger: "click",
-//		    startFun: function(i) {
-//		        jQuery(".focusBox .txts li").eq(i).animate({
-//		            "bottom": 0
-//		        }).siblings().animate({
-//		            "bottom": -36
-//		        });
-//		    }
-//		});
-//	
-//		
-//}
-//
+//	  	$(".footer").load("footer.html",function(){
+//	  	})
+	
+   
+	$(".slideBox").slide({
+    	mainCell: ".bd ul",
+    	autoPlay: true
+		});
+		jQuery(".focusBox").slide({
+		    titCell: ".num li",
+		    mainCell: ".pic",
+		    effect: "fold",
+		    autoPlay: true,
+		    trigger: "click",
+		    startFun: function(i) {
+		        jQuery(".focusBox .txts li").eq(i).animate({
+		            "bottom": 0
+		        }).siblings().animate({
+		            "bottom": -36
+		        });
+		    }
+		});
+	
+		
+}
+
 //if(/aboutus/.test(location.href)){
 //	$("#header").load("head.html",function(){
 //		
